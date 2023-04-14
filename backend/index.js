00589fb1e62e68c871d7499ac532ee7ff41ae58a
florianw830 <48376182+florianw830@users.noreply.github.com>
2023-04-14 13:26:52 +0000

    test

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,18 @@
-mport express from 'express';
+import express from 'express';
+import expressJwt from 'express-jwt'
 import DB from './db.js'
 
 const PORT = process.env.PORT || 3000;
-
+const pKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAyn2vP592Ju/iKXQW1DCrSTXyQXyo11Qed1SdzFWC+mRtdgioKibzYMBt2MfAJa6YoyrVNgOtGvK659MjHALtotPQGmis1VVvBeMFdfh+zyFJi8NPqgBTXz6bQfnu85dbxVAg95J+1Ud0m4IUXME1ElOyp1pi88+w0C6ErVcFCyEDS3uAajBY6vBIuPrlokbl6RDcvR9zX85s+R/s7JeP1XV/e8gbnYgZwxcn/6+7moHPDl4LqvVDKnDq9n4W6561s8zzw8EoAwwYXUC3ZPe2/3DcUCh+zTF2nOy8HiN808CzqLq1VeD13q9DgkAmBWFNSaXb6vK6RIQ9+zr2cwdXiwIDAQAB";
 /** Zentrales Objekt für unsere Express-Applikation */
 const app = express();
 app.use(express.json());
 /** global instance of our database */
 let db = new DB();
 
+app.use(
+    expressJwt({secret:pKey, algorithms:['HS256']})
+)
 /** Initialize database connection */
 async function initDB() {
     await db.connect();
